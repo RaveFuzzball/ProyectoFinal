f91351429c65cd2f406dcbcb5dea04f56d965e54
Ervey Guerrero <69262706+ErveyG@users.noreply.github.com>
2020-12-05 19:10:17 -0600

    Update Controller.java

--- Controllers/Controller.java
+++ Controllers/Controller.java
@@ -1,6 +1,57 @@
 package Controllers;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import GUI.MenuView;
+import GUI.BoletoInternacionalView;
+import GUI.BoletoNacionalView;
+import GUI.BoletoView;
+import GUI.MaletaView;
+
+import Modelo.Boleto;
+import Modelo.BoletoInternacional;
+import Modelo.BoletoNacional;
+import Modelo.Pasajero;
+import Modelo.PasajeroTurista;
+import Modelo.PasajeroEjecutivo;
+import Modelo.PasajeroPrimeraClase;
+import Modelo.EnumVisa;
+import Modelo.EnumVuelo;
+import Modelo.EnumClase;
+import Modelo.Maleta;
+
 
 public class Controller {
+
+/*Funcionalidad maleta*/
+
+    public class MaletaViewController {
+
+        private MaletaView vista;
+
+        public Maleta creaMaleta(double peso) {
+
+            return new Maleta(peso);
+        }
+
+        public Maleta creaMaleta() {
+
+            return new Maleta(this.vista.getPesoMaletaEntrada());
+        }
+
+        public void mostrarVista() throws Exception {
+
+            this.vista = new MaletaView();
+
+        }
+
+    }
+  
+  /*Fin cuncionaldiad maleta
+
+
+
 
 }
